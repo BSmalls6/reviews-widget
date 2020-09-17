688ad0d5c87dac079a120ca7f99bf1c1d62d5847
BSmalls6 <smallwoodbrennan@gmail.com>
2020-09-16 17:23:45 -0700

    adding beginning API code

---
+++ src/reviews.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+    event.preventDefault();
+    getReviews();
+})
+
+function getReviews() {
+    var location = [];
+    var accountId = [];
+
+    var queryUrl = "https://mybusiness.googleapis.com/v4/accounts/" + accountId + "/locations/" + location + "/reviews";
+
+    $.ajax({
+        url: queryUrl,
+        method: "GET"
+    }).then(function(allReviews) {
+        console.log(allReviews);
+
+    })
+}
